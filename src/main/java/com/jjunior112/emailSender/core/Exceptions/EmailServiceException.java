3d4efcb805bd67a871a01228a90e61c5a482c427
jjunior112 <juarezgcjr@gmail.com>
2025-07-09 16:59:38 -0300

    feat: criação de exceção personalizada para emailSender

---
+++ src/main/java/com/jjunior112/emailSender/core/Exceptions/EmailServiceException.java
@@ -0,0 +1,15 @@
+package com.jjunior112.emailSender.core.Exceptions;
+
+public class EmailServiceException extends RuntimeException{
+
+    public EmailServiceException(String message)
+    {
+        super(message);
+    }
+
+    public EmailServiceException(String message,Throwable cause)
+    {
+        super(message,cause);
+    }
+
+}
